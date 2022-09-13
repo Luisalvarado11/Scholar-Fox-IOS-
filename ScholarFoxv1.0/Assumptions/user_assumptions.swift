549780131023672e0477ddb5812b22efe6a70093
Marco Gabriel <marcog10@csu.fullerton.edu>
2022-09-13 12:59:11 -0700

    Created  a new branch Assumptions

---
+++ ScholarFoxv1.0/Assumptions/user_assumptions.swift
@@ -0,0 +1,20 @@
+//
+//  user_assumptions.swift
+//  ScholarFoxv1.0
+//
+//  Created by Marco Gabriel on 9/13/22.
+//
+
+import SwiftUI
+
+struct user_assumptions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct user_assumptions_Previews: PreviewProvider {
+    static var previews: some View {
+        user_assumptions()
+    }
+}
