b96cbe5db10070e661c0513e67ca9a62a508efa1
Marco Gabriel <marcog10@csu.fullerton.edu>
2022-09-20 13:14:35 -0700

    Added an image background

--- ScholarFoxv1.0/Assumptions/user_assumptions.swift
+++ ScholarFoxv1.0/Assumptions/user_assumptions.swift
@@ -15,7 +15,6 @@ struct schoolsInfo {
 }
 
 struct user_assumptions: View {
-    
     @State private var gpa: String = ""
     @State private var major: String = ""
     
@@ -30,8 +29,15 @@ struct user_assumptions: View {
     func assumeForGpa(_ assume: String) -> String {
         let user = Double(assume)
         var something = ""
+        
         if user == CSUF.GPA {
             something = CSUF.name
+        } else if user == CALPOLY.GPA {
+            something = CALPOLY.name
+        } else if user == CSULB.GPA {
+            something = CSULB.name
+        } else {
+            something = "Empty"
         }
         return String(something)
     }
@@ -40,48 +46,68 @@ struct user_assumptions: View {
     func assumeForMajor(_ assume: String) -> String {
         let user = major
         var something = ""
+        
         if user == CSUF.major {
             something = CSUF.name
+        } else if user == CALPOLY.major {
+            something = CALPOLY.name
+        } else if user == CSULB.major {
+            something = CSULB.name
+        } else {
+            something = "Empty"
         }
         return String(something)
     }
     
-    //shows the phone
+//        .background(
+//        Image("school_image")
+//            .resizable()
+//            .edgesIgnoringSafeArea(.all)
+//            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+//        )
+    
+    // shows the phone
     var body: some View {
-        VStack (alignment: .leading, spacing: 20) {
-            Text(" Assumptions Page")
-                .font(.title)
-                .foregroundColor(.black)
-                .fontWeight(.bold)
-            
-            // other code
-            VStack {
-                Form {
-                    Section(header: Text("GPA")) {
-                        TextField("Enter Your GPA:", text: $gpa)
-                            .keyboardType(.decimalPad)
-                        
-                    }
-                    .padding()
-                    
-                    Section(header: Text("Major")) {
-                        TextField("Enter your a major ", text: $major)
-                            .keyboardType(.default)
-                    }
-                    .padding()
-                    
-                    // header for school
-                    Section(header: Text("School")) {
-                        // first: takes the converted amount
-                        // second: displaying the information
-                        Text("\(assumeForGpa(gpa))")
-                        Text("\(assumeForMajor(major))")
-                    }
-                    .padding()
-                }
+        // other code
+        VStack {
+            VStack(alignment: .trailing, spacing: 20) {
+                Text(" Assumptions Page")
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .fontWeight(.black)
             }
+                
+            // background image
+            // work
+            Section(header: Text("GPA")) {
+                TextField("Enter Your GPA:", text: $gpa)
+                    .keyboardType(.decimalPad)
+            }
+            .padding()
+                    
+            Section(header: Text("Major")) {
+                TextField("Enter your a major ", text: $major)
+                    .keyboardType(.default)
+            }
+            .padding()
+    
+            // header for school
+            Section(header: Text("School")) {
+                // first: takes the converted amount
+                // second: displaying the information
+                Text("\(assumeForGpa(gpa))")
+                Text("\(assumeForMajor(major))")
+            }
+            .padding()
         }
-        .background(.gray)
+        .background(
+        Image("background")
+            .resizable()
+            .edgesIgnoringSafeArea(.all)
+            .scaledToFit()
+            .frame(width: 470, height: 800)
+        )
+        
     }
 }
         
@@ -91,6 +117,3 @@ struct user_assumptions_Previews: PreviewProvider {
             .previewInterfaceOrientation(.portrait)
     }
 }
-
-
-
